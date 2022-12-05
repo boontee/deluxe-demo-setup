a13c0be5c6d7096ebcc5897c539cd95671fad949
Tim Wuthenow <Tim.Wuthenow@ibm.com>
2022-12-05 13:19:28 -0500

    clone add

---
+++ clone.sh
@@ -0,0 +1,14 @@
+echo "Cloning Rest Services"
+git clone https://github.com/timwuthenow/deluxe-rest-services.git
+
+echo "Cloning Model Repository"
+git clone https://github.com/timwuthenow/deluxemodels.git 
+
+echo "Cloning Deluxe Process Repository"
+git clone https://github.com/timwuthenow/deluxeprocess.git
+
+echo "Cloning Deluxe Rules Repository"
+git clone https://github.com/timwuthenow/deluxerules.git 
+
+echo "Cloning UI project"
+git clone https://github.com/timwuthenow/employee-onboarding-ui.git
